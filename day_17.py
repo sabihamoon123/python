95bf790c97a24b808afc46d09bfd4504bf89d6ad
Sabiha Mahjabin Moon <115579991+sabihamoon123@users.noreply.github.com>
2025-02-09 00:22:01 +0600

    Add files via upload

---
+++ day_17.py
@@ -0,0 +1,23 @@
+class user:
+    
+    def __init__(self, user_id, username) -> None:
+        self.id = user_id
+        self.username = username
+        self.followers = 0
+        self.following = 0
+    
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
+
+user_1 = user("001","moon")
+
+user_2 = user("002","asif")
+
+user_1.follow(user_2)
+print(user_1.followers)
+print(user_1.following)
+print(user_2.followers)
+print(user_2.following)
+
+
