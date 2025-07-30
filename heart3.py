7d51c57f4c27f079e11225cec309d3eb455b7352
Sabiha Mahjabin Moon <115579991+sabihamoon123@users.noreply.github.com>
2025-07-31 00:52:24 +0600

    Add files via upload

---
+++ heart3.py
@@ -0,0 +1,27 @@
+import turtle
+
+t=turtle.Turtle()
+s=turtle.Turtle()
+
+t.hideturtle()
+t.goto(0, -10)
+
+
+t.pensize(3)
+t.color('red')
+t.begin_fill()
+t.left(140)
+t.forward(180)
+t.circle(-90,200)
+t.setheading(60)
+t.circle(-90,200)
+t.forward(178)
+t.end_fill()
+
+t.penup()
+
+t.goto(-132, 130)
+t.pendown()
+t.color('white')
+t.write("I LOVE YOU ASUU!", font=("Verdana",20,"bold"))
+turtle.done()
